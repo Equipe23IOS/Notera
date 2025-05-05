854ae60d9a9738c374c074ba670c1bb057e72c05
Beuuuh <belldiscord8768@hotmail.com>
2025-05-05 16:11:45 -0300

    Adicionado navegação inicial e tela de criação de entrada com botão Save

--- Diario/Diario/ContentView.swift
+++ Diario/Diario/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                HStack {
+                    Text("Entrys")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: {
+                        Diary()
+                    }, label: {
+                        Text("New")
+                    })
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct Diary: View {
+    @State private var diaryTitle: String = ""
+    @State private var diaryEntry: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Title", text: $diaryTitle)
+                .font(.title)
+            
+            TextField("Write your ideas", text: $diaryEntry)
+            
+            Spacer()
         }
         .padding()
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Save") {
+                    print("Salvar: \(diaryTitle) - \(diaryEntry)")
+                }
+            }
+        }
     }
 }
 
