a5b3d765adf2f56f8ae8753f80b791d9dc92346a
Beuuuh <belldiscord8768@hotmail.com>
2025-07-27 20:29:28 -0300

    Feat: melhoria nos textos de onBoarding

--- Diario/Diario/Resources/WelcomeData.swift
+++ Diario/Diario/Resources/WelcomeData.swift
@@ -10,9 +10,10 @@ import SwiftUI
 
 struct WelcomeData {
     static let pages = [
-        PageInfo(label: "Welcome to Notera", text: "A calm, safe, and personal space. Here, you can write everything you feel and think."),
-        PageInfo(label: "Record your day freely", text: "Jot down ideas, thoughts, victories, or just a random reflection. No rules."),
+        PageInfo(label: "Welcome to Notera", text: "Notera aims to be a safe space for your records, thoughts and emotions"),
+        PageInfo(label: "What are notebooks?", text: "You may document anything you please in notebooks, and if you feel like you need some more organization, create a new one!"),
         PageInfo(label: "Your notes are private", text: "Everything you write in Notera is protected and accessible only to you."),
-        PageInfo(label: "Begin your personal journey", text: "Take the first step. Writing can transform your days — one moment at a time.")
+        PageInfo(label: "Capture your emotions!", text: "Keeping track of your feelings’s also important! For that, use our Humortracking feature!"),
+        PageInfo(label: "Begin your personal journey", text: "Take the first step. Writing can transform your days ― one moment at a time.")
     ]
 }
