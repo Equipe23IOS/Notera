3240613fafe09a09b2f73d083d3ea1126383c56a
Beuuuh <belldiscord8768@hotmail.com>
2025-07-26 22:10:05 -0300

    Feat: Adicionada PasswordValidationView

---
+++ Diario/Diario/Views/PasswordValidationView.swift
@@ -0,0 +1,56 @@
+//
+//  PasswordValidationView.swift
+//  Diario
+//
+//  Created by iredefbmac_36 on 26/07/25.
+//
+
+import SwiftUI
+
+struct PasswordValidationView: View {
+    @State var password: String = ""
+    @State var isSecure: Bool = false
+    @State var showPopup: Bool = false
+    @State var goToNextPage: Bool = false
+    @ObservedObject var passwordViewModel: PasswordViewModel
+    
+    var body: some View {
+        Text("Welcome back!")
+        
+        ZStack {
+            Group {
+                if(isSecure) {
+                    TextField("Enter a password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                } else {
+                    SecureField("Enter a password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                }
+            }
+            
+            HStack {
+                Spacer()
+                
+                Button(action: {
+                    isSecure.toggle()
+                }, label: {
+                    Image(systemName: isSecure ? "eye" : "eye.slash")
+                })
+            }
+            .padding()
+            .padding(.trailing, 10)
+        }
+        
+        Button(action: {
+            if(passwordViewModel.validatePasswrd(password)) {
+                goToNextPage.toggle()
+            } else {
+                showPopup.toggle()
+            }
+        }, label: {
+            Text("Log in")
+        })
+    }
+}
