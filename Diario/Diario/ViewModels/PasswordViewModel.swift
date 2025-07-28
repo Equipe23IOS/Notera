2adcb905988507926d7608c98992a535234f30d3
Beuuuh <belldiscord8768@hotmail.com>
2025-07-28 10:55:50 -0300

    Feat: senhas agora sao criptografadas e funcao de mudanca de senha adicionada

--- Diario/Diario/ViewModels/PasswordViewModel.swift
+++ Diario/Diario/ViewModels/PasswordViewModel.swift
@@ -6,15 +6,20 @@
 //
 
 import SwiftUI
+import KeychainSwift
 
 class PasswordViewModel: ObservableObject {
-    var userPassword: String = ""
+    let keychain = KeychainSwift()
     
     func createPassword(_ password: String) {
-        userPassword = password
+        keychain.set(password, forKey: "userPassword")
     }
     
     func validatePasswrd(_ password: String) -> Bool {
-        return password == userPassword
+        return keychain.get("userPassword") == password
+    }
+    
+    func changePassword(_ newPassword: String) {
+        keychain.set(newPassword, forKey: "userPassword")
     }
 }
