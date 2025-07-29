5ad3bc71ef582e3c3882345e6c7f27bf54453dfc
MP2066-maker <santanabenoit26@gmail.com>
2025-07-29 18:25:37 -0300

    Style: polimento na WelcomeView

--- Diario/Diario/Resources/WelcomeData.swift
+++ Diario/Diario/Resources/WelcomeData.swift
@@ -12,7 +12,7 @@ struct WelcomeData {
     static let pages = [
         PageInfo(label: "Welcome to Notera", text: "Notera aims to be a safe space for your records, thoughts and emotions"),
         PageInfo(label: "What are notebooks?", text: "You may document anything you please in notebooks, and if you feel like you need some more organization, create a new one!"),
-        PageInfo(label: "Your notes are private", text: "Everything you write in Notera is protected and accessible only to you."),
+        PageInfo(label: "Your notes\nare private", text: "Everything you write in Notera is protected and accessible only to you."),
         PageInfo(label: "Capture your emotions!", text: "Keeping track of your feelings’s also important! For that, use our Humortracking feature!"),
         PageInfo(label: "Begin your personal journey", text: "Take the first step. Writing can transform your days ― one moment at a time.")
     ]
--- Diario/Diario/Views/WelcomeView.swift
+++ Diario/Diario/Views/WelcomeView.swift
@@ -9,37 +9,52 @@ import SwiftUI
 
 struct WelcomeView: View {
     @AppStorage("hasSeenWelcome") var hasSeenWelcome: Bool = false
+    
     var body: some View {
-        VStack {
-            TabView {
-                ForEach(WelcomeData.pages) { page in
-                    VStack {
-                        Text(page.label)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .multilineTextAlignment(.center)
+        ZStack {
+            Color.canvas
+                .ignoresSafeArea()
+            
+            VStack {
+                TabView {
+                    ForEach(WelcomeData.pages) { page in
+                        VStack {
+                            Text(page.label)
+                                .font(.custom("Leorio", size: 36))
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.caramel)
 
-                        Text(page.text)
-                            .fontWeight(.medium)
-                            .multilineTextAlignment(.center)
-                            .padding()
+                            Text(page.text)
+                                .font(.custom("Leorio", size: 20))
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .foregroundColor(.caramel)
+                        }
                     }
                 }
-            }
 
-            Button {
-                hasSeenWelcome = true
-            } label: {
-                Text("Get Started")
-                    .font(.title3)
-                    .fontWeight(.medium)
+                Button {
+                    hasSeenWelcome = true
+                } label: {
+                    Capsule()
+                        .fill(Color.toast)
+                        .frame(width: 160, height: 40)
+                        .overlay() {
+                            Text("Get Started")
+                                .padding()
+                                .font(.title3)
+                                .fontWeight(.medium)
+                                .foregroundColor(.linen)
+                        }
+                }
             }
-        }
-        .tabViewStyle(.page)
-        .onAppear {
-            UIPageControl.appearance().currentPageIndicatorTintColor = .label
-            UIPageControl.appearance().pageIndicatorTintColor = .systemGray
+            .tabViewStyle(.page)
+            .onAppear {
+                UIPageControl.appearance().currentPageIndicatorTintColor = .label
+                UIPageControl.appearance().pageIndicatorTintColor = .caramel
+            }
         }
     }
 }
-
