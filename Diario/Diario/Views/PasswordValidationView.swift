b2cb424c61a505c3da90cc40f5642e1f0406e81b
MP2066-maker <santanabenoit26@gmail.com>
2025-08-01 18:15:45 -0300

    Style: polimento na PasswordValidationView

--- Diario/Diario/Views/PasswordValidationView.swift
+++ Diario/Diario/Views/PasswordValidationView.swift
@@ -15,42 +15,72 @@ struct PasswordValidationView: View {
     @ObservedObject var passwordViewModel: PasswordViewModel
     
     var body: some View {
-        Text("Welcome back!")
-        
         ZStack {
-            Group {
-                if(isSecure) {
-                    TextField("Enter a password", text: $password)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
-                } else {
-                    SecureField("Enter a password", text: $password)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
-                }
-            }
+            Color.canvas
+                .ignoresSafeArea()
             
-            HStack {
-                Spacer()
+            VStack {
+                Text("Welcome back!")
+                    .font(.custom("Leorio", size: 24))
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundColor(.caramel)
+                
+                ZStack {
+                    Group {
+                        if(isSecure) {
+                            TextField("Enter a password", text: $password)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+                                .font(.custom("Leorio", size: 16))
+                        } else {
+                            SecureField("Enter a password", text: $password)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+                                .font(.custom("Leorio", size: 16))
+                        }
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            isSecure.toggle()
+                        }, label: {
+                            Image(systemName: isSecure ? "eye" : "eye.slash")
+                                .foregroundColor(.toast)
+                        })
+                    }
+                    .padding()
+                    .padding(.trailing, 10)
+                }
                 
                 Button(action: {
-                    isSecure.toggle()
+                    if(passwordViewModel.validatePasswrd(password)) {
+                        goToNotera.toggle()
+                    } else {
+                        showPopup.toggle()
+                    }
                 }, label: {
-                    Image(systemName: isSecure ? "eye" : "eye.slash")
+                    Capsule()
+                        .fill(Color.toast)
+                        .frame(width: 160, height: 40)
+                        .overlay() {
+                            Text("Log in")
+                                .font(.custom("Leorio", size: 20))
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .foregroundColor(.canvas)
+                        }
                 })
             }
-            .padding()
-            .padding(.trailing, 10)
         }
-        
-        Button(action: {
-            if(passwordViewModel.validatePasswrd(password)) {
-                goToNotera.toggle()
-            } else {
-                showPopup.toggle()
-            }
-        }, label: {
-            Text("Log in")
-        })
     }
+}
+
+#Preview {
+    @Previewable @State var PH = false
+    PasswordValidationView(goToNotera: $PH, passwordViewModel: PasswordViewModel())
 }
