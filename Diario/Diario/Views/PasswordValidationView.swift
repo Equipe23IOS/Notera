7d970408457e5633f60d8590a7584099a0592cd8
Beuuuh <belldiscord8768@hotmail.com>
2025-08-02 13:33:40 -0300

    Feat: adiciona uma tela de bloqueio de 30 segundos ao errar a senha

--- Diario/Diario/Views/PasswordValidationView.swift
+++ Diario/Diario/Views/PasswordValidationView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct PasswordValidationView: View {
+    @State var attempts: Int = 1
     @State var password: String = ""
     @State var isSecure: Bool = false
     @State var showPopup: Bool = false
@@ -15,83 +16,108 @@ struct PasswordValidationView: View {
     @Binding var goToNotera: Bool
     @ObservedObject var passwordViewModel: PasswordViewModel
     
+    func countdown() {
+        var remainingTime = 10
+        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { t in
+            if(remainingTime == 0) {
+                t.invalidate()
+                attempts += 1
+            }
+            remainingTime -= 1
+        }
+    }
+    
     var body: some View {
         ZStack {
             Color.canvas
                 .ignoresSafeArea()
             
-            VStack {
-                if(hasJustCreatedPassword) {
-                    Text("Insert the password you just created")
-                        .font(.custom("Leorio", size: 24))
-                        .fontWeight(.medium)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                        .foregroundColor(.caramel)
-                } else {
-                    Text("Welcome back!")
-                        .font(.custom("Leorio", size: 24))
-                        .fontWeight(.medium)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                        .foregroundColor(.caramel)
-                }
+            if(attempts % 5 == 0) {
+                EmptyView()
                 
-                ZStack {
-                    Group {
-                        if(isSecure) {
-                            TextField("Enter a password", text: $password)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .padding()
-                                .font(.custom("Leorio", size: 16))
-                        } else {
-                            SecureField("Enter a password", text: $password)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .padding()
-                                .font(.custom("Leorio", size: 16))
-                        }
+                Text("\(countdown())")
+                    .hidden()
+                
+                Text("Try again in 30 seconds")
+                    .font(.custom("Leorio", size: 24))
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundColor(.caramel)
+                
+            } else {
+                VStack {
+                    if(hasJustCreatedPassword) {
+                        Text("Insert the password you just created")
+                            .font(.custom("Leorio", size: 24))
+                            .fontWeight(.medium)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .foregroundColor(.caramel)
+                    } else {
+                        Text("Welcome back!")
+                            .font(.custom("Leorio", size: 24))
+                            .fontWeight(.medium)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .foregroundColor(.caramel)
                     }
                     
-                    HStack {
-                        Spacer()
-                        
-                        Button(action: {
-                            isSecure.toggle()
-                        }, label: {
-                            Image(systemName: isSecure ? "eye" : "eye.slash")
-                                .foregroundColor(.toast)
-                        })
-                    }
-                    .padding()
-                    .padding(.trailing, 10)
-                }
-                
-                Button(action: {
-                    if(passwordViewModel.validatePasswrd(password)) {
-                        goToNotera.toggle()
-                    } else {
-                        showPopup.toggle()
-                    }
-                }, label: {
-                    Capsule()
-                        .fill(Color.toast)
-                        .frame(width: 160, height: 40)
-                        .overlay() {
-                            Text("Log in")
-                                .font(.custom("Leorio", size: 20))
-                                .fontWeight(.medium)
-                                .multilineTextAlignment(.center)
-                                .padding()
-                                .foregroundColor(.canvas)
+                    ZStack {
+                        Group {
+                            if(isSecure) {
+                                TextField("Enter a password", text: $password)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .padding()
+                                    .font(.custom("Leorio", size: 16))
+                            } else {
+                                SecureField("Enter a password", text: $password)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .padding()
+                                    .font(.custom("Leorio", size: 16))
+                            }
                         }
-                })
-                .alert("Error", isPresented: $showPopup) {
-                                    Button("OK", role: .cancel) { }
-                                } message: {
-                                    Text("The password is wrong")
-                                }
+                        
+                        HStack {
+                            Spacer()
+                            
+                            Button(action: {
+                                isSecure.toggle()
+                            }, label: {
+                                Image(systemName: isSecure ? "eye" : "eye.slash")
+                                    .foregroundColor(.toast)
+                            })
+                        }
+                        .padding()
+                        .padding(.trailing, 10)
+                    }
+                    Button(action: {
+                        if(passwordViewModel.validatePasswrd(password)) {
+                            goToNotera.toggle()
+                        } else {
+                            attempts += 1
+                            showPopup.toggle()
+                        }
+                    }, label: {
+                        Capsule()
+                            .fill(Color.toast)
+                            .frame(width: 160, height: 40)
+                            .overlay() {
+                                Text("Log in")
+                                    .font(.custom("Leorio", size: 20))
+                                    .fontWeight(.medium)
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                                    .foregroundColor(.canvas)
+                            }
+                    })
+                    .alert("Error", isPresented: $showPopup) {
+                        Button("OK", role: .cancel) { }
+                    } message: {
+                        Text("The password is wrong")
+                    }
+                }
             }
         }
     }
 }
-
