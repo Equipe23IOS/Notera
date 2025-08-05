fc22b7ea8f7f08e705fed96b9cbe6b8c4204bb26
Beuuuh <belldiscord8768@hotmail.com>
2025-08-05 19:31:46 -0300

    Feat: adiciona selecao de sprites com highlights visuais

--- Diario/Diario/Views/CreationNotebookView.swift
+++ Diario/Diario/Views/CreationNotebookView.swift
@@ -11,12 +11,30 @@ struct CreationNotebookView: View{
     @Binding var activateSheet: Bool
     @State var notebookName: String = ""
     @State var showPopup: Bool = false
+    @State var selectedSprite: String = ""
     @ObservedObject var notebookViewModel: NotebooksViewModel
     
+    
+    func loadSprites() -> some View {
+        ForEach(NotebookSprites.notebookSprites, id: \.self) { i in
+            Image(i)
+                .padding()
+                .onTapGesture() {
+                    selectedSprite = i
+                    print(i)
+                }
+                .overlay() {
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(selectedSprite == i ? Color.canvas : Color.clear, lineWidth: 4)
+                        .padding(.all, 6)
+                }
+        }
+    }
+    
     var body:  some View {
-        ZStack{
+        ZStack {
             Color(.linen)
-            VStack{
+            VStack {
                 Text("New Notebook")
                     .foregroundColor(.espresso)
                     .font(.custom("Leorio", size: 36))
@@ -26,7 +44,13 @@ struct CreationNotebookView: View{
                 TextField("Notebook Name", text: $notebookName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-                    .font(.custom("Leorio", size: 12))
+                    .font(.custom("Leorio", size: 20))
+                
+                Text("Which notebook do you want?")
+                    .foregroundColor(.espresso)
+                    .font(.custom("Leorio", size: 24))
+                    .fontWeight(.bold)
+                    .padding()
                 
                 ScrollView(.horizontal) {
                     HStack {
@@ -39,7 +63,7 @@ struct CreationNotebookView: View{
                         showPopup.toggle()
                     } else {
                         activateSheet.toggle()
-                        notebookViewModel.createNotebook(notebookName)
+                        notebookViewModel.createNotebook(notebookName, selectedSprite)
                     }
                 }, label: {
                     Capsule()
