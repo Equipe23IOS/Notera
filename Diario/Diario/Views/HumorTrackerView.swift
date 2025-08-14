86ebe716666bc7ff86608df405d57b1dc486299b
Beuuuh <belldiscord8768@hotmail.com>
2025-08-14 08:36:30 -0300

    Feat: limita a navegacao do usuario no calendario e corrige alguns pequenos problemas

--- Diario/Diario/Views/HumorTrackerView.swift
+++ Diario/Diario/Views/HumorTrackerView.swift
@@ -10,11 +10,26 @@ import HorizonCalendar
 
 struct HumorTrackerView: View {
     let calendar: Calendar = Calendar.current
-    var todaysDate: Date = Calendar.current.date(from: DateComponents(month: Calendar.current.component(.month, from: Date()), day: Calendar.current.component(.day, from: Date())))!
     @AppStorage("startDate") var startDate: Date = Calendar.current.date(from: DateComponents(month: Calendar.current.component(.month, from: Date())))!
     @State var selectedDate: Date?
+    @State var visibleDate: Date = Date()
     @State var activateSheet: Bool = false
     @StateObject var humorTrackerViewModel: HumorTrackerViewModel = HumorTrackerViewModel()
+    
+    func goBackAMonth() {
+        guard let newMonth = Calendar.current.date(byAdding: .month, value: -1, to: visibleDate) else {
+            return
+        }
+        visibleDate = newMonth
+    }
+    
+    func goForwardOneMonth() {
+        guard let newMonth = Calendar.current.date(byAdding: .month, value: 1, to: visibleDate) else {
+            return
+        }
+        visibleDate = newMonth
+    }
+    
     
     var body: some View {
         ZStack {
@@ -22,7 +37,7 @@ struct HumorTrackerView: View {
                 .ignoresSafeArea()
             
             VStack {
-                CalendarViewRepresentable(calendar: calendar, visibleDateRange: startDate...todaysDate, monthsLayout: .vertical(options: VerticalMonthsLayoutOptions()), dataDependency: nil)
+                CalendarViewRepresentable(calendar: calendar, visibleDateRange: visibleDate...visibleDate, monthsLayout: .vertical(options: VerticalMonthsLayoutOptions()), dataDependency: nil)
                     .verticalDayMargin(16)
                     .days() { day in
                         VStack {
@@ -30,7 +45,7 @@ struct HumorTrackerView: View {
                                 .stroke(.espresso, lineWidth: 2)
                                 .frame(width: 40, height: 40)
                                 .overlay() {
-                                    Image(systemName: calendar.component(.day, from: todaysDate) >= day.day ? "plus" : "")
+                                    Image(systemName: calendar.component(.day, from: visibleDate) >= day.day ? "plus" : "")
                                 }
                           
                             Text(String(day.day))
@@ -46,8 +61,9 @@ struct HumorTrackerView: View {
                             Spacer()
                             
                             Button(action: {
-                                if(todaysDate < startDate) {
-                                    
+                                //Lembrar de criar uma variavel dos componentes de visibleDate para melhorar a leitura do codigo
+                                if(calendar.dateComponents([.month], from: visibleDate).month! != calendar.dateComponents([.month], from: startDate).month!) {
+                                    goBackAMonth()
                                 }
                             }, label: {
                                 Circle()
@@ -55,7 +71,7 @@ struct HumorTrackerView: View {
                                     .frame(width: 40)
                                     .overlay() {
                                         Image(systemName: "chevron.left")
-                                            .foregroundColor(todaysDate < startDate ? .toast : .canvas)
+                                            .foregroundColor(calendar.dateComponents([.month], from: visibleDate) != calendar.dateComponents([.month], from: startDate) ? .toast : .canvas)
                                             
                                     }
                             })
@@ -72,8 +88,8 @@ struct HumorTrackerView: View {
                                 .padding(.top, 16)
                             
                             Button(action: {
-                                if(calendar.date(from: month.components)! > todaysDate) {
-                                    
+                                if(calendar.dateComponents([.month], from: visibleDate).month! > calendar.dateComponents([.month], from: startDate).month!) {
+                                    goForwardOneMonth()
                                 }
                             }, label: {
                                 Circle()
@@ -81,7 +97,7 @@ struct HumorTrackerView: View {
                                     .frame(width: 40)
                                     .overlay() {
                                         Image(systemName: "chevron.right")
-                                            .foregroundColor(calendar.date(from: month.components)! > todaysDate ? .toast : .canvas)
+                                            .foregroundColor(calendar.dateComponents([.month], from: visibleDate) != calendar.dateComponents([.month], from: startDate) ? .toast : .canvas)
                                     }
                             })
                             .padding(.leading, -16)
@@ -94,7 +110,7 @@ struct HumorTrackerView: View {
                         activateSheet.toggle()
                     }
                     .backgroundColor(.linen)
-                    .frame(height: 560)
+                    .frame(height: 640)
                     .cornerRadius(20)
                     .padding()
                 
