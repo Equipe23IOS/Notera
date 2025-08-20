769c194977ecb0da2eaec47954331fd4ce3ba26c
Beuuuh <belldiscord8768@hotmail.com>
2025-08-20 17:07:28 -0300

    Feat: adiciona DashboardView

--- Diario/Diario/Views/DashboardView.swift
+++ Diario/Diario/Views/DashboardView.swift
@@ -6,13 +6,48 @@
 //
 
 import SwiftUI
+import Charts
 
 struct DashboardView: View {
-    var body: some View {
-        Text("pode pegar")
+    var data: [(name: String, amount: Int)] {
+        humorTrackerViewModel.getInfo()
     }
-}
-
-#Preview {
-    DashboardView()
+    
+    @ObservedObject var humorTrackerViewModel: HumorTrackerViewModel
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.linen)
+                .frame(width: 400, height: 440)
+            
+            VStack {
+                Chart(data, id: \.name) { name, amount in
+                    SectorMark(
+                        angle: .value("Amount", amount),
+                        innerRadius: .ratio(0.5)
+                    )
+                    .foregroundStyle(by: .value("Categoria", name))
+                }
+                .frame(width: 300, height: 300)
+                .chartForegroundStyleScale([
+                    "Very Sad": Color(red: 30/255, green: 58/255, blue: 95/255),
+                    "Sad": Color(red: 59/255, green: 130/255, blue: 181/255),
+                    "Neutral": Color(red: 92/255, green: 92/255, blue: 92/255),
+                    "Happy": Color(red: 230/255, green: 138/255, blue: 46/255),
+                    "Very Happy": Color(red: 2255/255, green: 210/255, blue: 63/255)
+                ])
+                
+                
+                HStack {
+                    ForEach(CalendarResources.emojis.indices, id: \.self) { i in
+                        Image(CalendarResources.emojis[i])
+                            .padding(.all, 4)
+                    }
+                }
+            }
+        }
+        
+        
+    }
 }
