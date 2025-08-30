611a9cd0c952e9eb896972bb6a101470a737460e
MP2066-maker <santanabenoit26@gmail.com>
2025-08-30 14:21:29 -0300

    Feat: adiciona SideBarMenuView

---
+++ Diario/Diario/Views/SideBarMenuView.swift
@@ -0,0 +1,36 @@
+//
+//  SideBarMenu.swift
+//  Diario
+//
+//  Created by iredefbmac_36 on 30/08/25.
+//
+
+import SwiftUI
+
+struct SideBarMenuView: View {
+    @Binding var sidebarIsOpened: Bool
+    
+    var body: some View {
+        if(sidebarIsOpened) {
+            NavigationStack {
+                ZStack {
+                    Rectangle()
+                        .opacity(0.4)
+                        .ignoresSafeArea()
+                        .onTapGesture {
+                            sidebarIsOpened.toggle()
+                        }
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            SideBarView()
+                        }
+                        .frame(width: 300)
+                        
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}
