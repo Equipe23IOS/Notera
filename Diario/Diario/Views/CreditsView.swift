4cd9b50d1f1af11139b8b894c365f5ac53dea4c2
Beuuuh <belldiscord8768@hotmail.com>
2025-09-02 11:25:45 -0300

    Style: polimento em CreditsView

--- Diario/Diario/Views/CreditsView.swift
+++ Diario/Diario/Views/CreditsView.swift
@@ -13,61 +13,67 @@ struct CreditsView: View {
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 12) {
-                TitleComponent(title: "Credits", weight: .bold, size: 36)
-                    .padding(.bottom, 20)
+            ZStack {
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
                 
-                TitleComponent(title: "A product of:", weight: .bold, size: 36)
-                TextComponent(text: "Notera", size: 28)
-                    .padding(.bottom, 20)
                 
-                TitleComponent(title: "Developed by:", weight: .bold, size: 36)
-                TextComponent(text: "Bell Marques", size: 28)
-                    .padding(.bottom, 20)
-                
-                TitleComponent(title: "UI/UX design:", weight: .bold, size: 36)
-                TextComponent(text: "Atila Benoit", size: 28)
-                    .padding(.bottom, 20)
-                
-                TitleComponent(title: "Support:", weight: .bold, size: 36)
-                TextComponent(text: "Guilherme Alfredo", size: 28)
-                    .padding(.bottom, 20)
-                
-                TitleComponent(title: "Art:", weight: .bold, size: 36)
-                TextComponent(text: "Bell Marques", size: 28)
-                    .padding(.bottom, 20)
-                
-                TitleComponent(title: "Special thanks to", weight: .bold, size: 36)
-                TextComponent(text: "My dearest friends", size: 28)
-                TextComponent(text: "Gleide Dias", size: 28)
-                TextComponent(text: "Lavyne Dias", size: 28)
-                TextComponent(text: "Edimario Mendes", size: 28)
-                    .padding(.bottom, 20)
-                
-                TextComponent(text: "© 2025 Notera. All Rights Reserved.", size: 20)
-                    .multilineTextAlignment(.center)
-            }
-            .frame(maxWidth: .infinity, maxHeight: 200)
-            .offset(y: animation ? 0 : 1000)
-            .animation(.linear(duration: 8), value: animation)
-            .navigationBarBackButtonHidden(true)
-            .onAppear() {
-                animation.toggle()
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        dismiss()
-                    }, label: {
-                        HStack {
-                            Image(systemName: "chevron.left")
-                                .foregroundColor(.toast)
-                            
-                            Text("Back")
-                                .foregroundColor(.toast)
-                                .font(.custom("Leorio", size: 20))
-                        }
-                    })
+                VStack(spacing: 12) {
+                    TitleComponent(title: "Credits", weight: .bold, size: 36)
+                        .padding(.bottom, 20)
+                    
+                    TitleComponent(title: "A product of:", weight: .bold, size: 36)
+                    TextComponent(text: "Notera", size: 28)
+                        .padding(.bottom, 20)
+                    
+                    TitleComponent(title: "Developed by:", weight: .bold, size: 36)
+                    TextComponent(text: "Bell Marques", size: 28)
+                        .padding(.bottom, 20)
+                    
+                    TitleComponent(title: "UI/UX design:", weight: .bold, size: 36)
+                    TextComponent(text: "Atila Benoit", size: 28)
+                        .padding(.bottom, 20)
+                    
+                    TitleComponent(title: "Support:", weight: .bold, size: 36)
+                    TextComponent(text: "Guilherme Alfredo", size: 28)
+                        .padding(.bottom, 20)
+                    
+                    TitleComponent(title: "Art:", weight: .bold, size: 36)
+                    TextComponent(text: "Bell Marques", size: 28)
+                        .padding(.bottom, 20)
+                    
+                    TitleComponent(title: "Special thanks to", weight: .bold, size: 36)
+                    TextComponent(text: "My dearest friends", size: 28)
+                    TextComponent(text: "Gleide Dias", size: 28)
+                    TextComponent(text: "Lavyne Dias", size: 28)
+                    TextComponent(text: "Edimario Mendes", size: 28)
+                        .padding(.bottom, 20)
+                    
+                    TextComponent(text: "© 2025 Notera. All Rights Reserved.", size: 20)
+                        .multilineTextAlignment(.center)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 200)
+                .offset(y: animation ? 0 : 1000)
+                .animation(.linear(duration: 8), value: animation)
+                .navigationBarBackButtonHidden(true)
+                .onAppear() {
+                    animation.toggle()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            dismiss()
+                        }, label: {
+                            HStack {
+                                Image(systemName: "chevron.left")
+                                    .foregroundColor(Color("ButtonColor"))
+                                
+                                Text("Back")
+                                    .foregroundColor(Color("ButtonColor"))
+                                    .font(.custom("Leorio", size: 20))
+                            }
+                        })
+                    }
                 }
             }
         }
