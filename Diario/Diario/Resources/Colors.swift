aea8a0af764bd8c55c807d440789cd9a9c5f553d
Beuuuh <belldiscord8768@hotmail.com>
2025-09-02 11:44:49 -0300

    Feat: corrige alguns erros e adiciona struct para deixar o style mais facil

---
+++ Diario/Diario/Resources/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  Diario
+//
+//  Created by iredefbmac_36 on 02/09/25.
+//
+
+import SwiftUI
+
+struct Colors {
+    static let toolbarColor: Color = Color("ToolbarColor")
+    static let backgroundColor: Color = Color("BackgroundColor")
+    static let buttonColor: Color = Color("ButtonColor")
+    static let buttonTextColor: Color = Color("ButtonTextColor")
+    static let textColor: Color = Color("TextColor")
+    static let shadingColor: Color = Color("ShadingColor")
+}
